f6479fc1a1a916e7644482405d16c3af259a26b1
E-10000 <742891270@qq.com>
2020-10-26 22:36:06 +0800

    后台java

---
+++ src/main/java/com/qiang/springvue/SpringVueApplication.java
@@ -0,0 +1,13 @@
+package com.qiang.springvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueApplication.class, args);
+    }
+
+}
